1358bc7e8459aa039740804e5132a0c771bdb1c0
Victor Barros <victorjuda49@gmail.com>
2022-10-25 17:47:56 -0300

    fix: name, decoration

--- lib/screens/form_tela.dart
+++ lib/screens/form_tela.dart
@@ -22,9 +22,23 @@ class _FormTelaState extends State<FormTela> {
               color: Colors.black12,
               borderRadius: BorderRadius.circular(10),
               border: Border.all(width: 3)),
-        child: Column(
-          children: [],
-        ),),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    hintText: 'Nome',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
